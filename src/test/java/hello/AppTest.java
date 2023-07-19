6d4926746f28e4d4ed7fdde369b16347d7414a19
Prajwal <prajwalnkathar46@gmail.com>
2023-07-19 16:53:11 +0530

    inital commit

---
+++ src/test/java/hello/AppTest.java
@@ -0,0 +1,30 @@
+package hello;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+
+public class AppTest {
+
+    @Test
+    public void test1() {
+    test1 t=new test1();
+        String expected = "madam";
+        String actual =t.palindrome(expected);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+    @Test
+    public void test18() {
+    test1 t=new test1();
+        String expected = "beer";
+        String actual =t.palindrome(expected);
+
+        Assertions.assertEquals("reeb", actual);
+    }
+    }
